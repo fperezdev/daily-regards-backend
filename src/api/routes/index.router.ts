a410debf31285698292f6307d27331b7a945113e
Francisco Perez Lefiman <fperez@wird.ai>
2024-01-06 10:35:51 -0300

    Agregar rate limit

--- src/api/routes/index.router.ts
+++ src/api/routes/index.router.ts
@@ -1,7 +1,18 @@
 import { Router } from 'express';
+import { rateLimit } from 'express-rate-limit';
 import messageRouter from './message.router';
 
 const router = Router();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 30 minutes
+  limit: 10, // Limit each IP to 10 requests per `window` (here, per 15 minutes).
+  standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Use an external store for consistency across multiple server instances.
+});
+
+router.use(limiter);
 
 router.get('/', (_req, res) => {
   res.send('Daily Regards Server');
